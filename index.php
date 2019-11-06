2a38ddf4c50502185c6ae889918cd842e2e81756
Renato_O <renatorsdeoliveira@gmail.com>
2019-11-06 10:41:07 -0300

    Criado formulário

--- index.php
+++ index.php
@@ -6,14 +6,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="<?= site_url() ;?>css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= site_url() ;?>css/base.css">
     <title>Crud PHP</title>
 </head>
 <body>
 
     <div class="container">
         <div class="row">
-            <div class="offset-md-4 col-md-8 ">
-               
+            <div class="offset-md-3 col-md-6 ">
+               <form action="" method="post">
+                    <div class="form-group">
+                        <input class="form-control"  type="text" name="nome" id="nome" placeholder="Digite seu nome" required>
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control"  type="email" name="email" id="email" placeholder="Digite seu e-mail" required>
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <input class="form-control"  type="text" name="profissao" id="profissao" placeholder="Digite sua profissão" required>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <input class="form-control"  type="tel" name="telefone" id="telefone" placeholder="Digite seu telefone" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <input type="file" class="form-control-file" id="arquivo">
+                    </div>
+                    <div class="text-center">
+                    <button type="submit" class="btn btn-primary">enviar</button>
+                    </div>
+               </form>
             </div>
         </div>
     </div>
