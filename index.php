6663c73a869ff159bd1ed2391f368a7198772406
Renato_O <renatorsdeoliveira@gmail.com>
2019-11-06 09:13:31 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <title>Crud PHP</title>
+</head>
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="offset-md-4 col-md-8 ">
+               
+            </div>
+        </div>
+    </div>
+
+    <script src="javascript/bootstrap.min.js"></script>
+</body>
+</html>
