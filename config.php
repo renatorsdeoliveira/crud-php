532c5f90b99102acd49a200a53640bbbb42f5a3a
Renato_O <renatorsdeoliveira@gmail.com>
2019-11-06 09:18:34 -0300

    criado função site_url e aplicado no front

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php 
+    function site_url($link = '') {
+        echo "http://localhost/crud-php/".$link;
+    }
+?>
--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-
+<?php include_once('config.php'); ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= site_url() ;?>css/bootstrap.min.css">
     <title>Crud PHP</title>
 </head>
 <body>
@@ -18,6 +18,6 @@
         </div>
     </div>
 
-    <script src="javascript/bootstrap.min.js"></script>
+    <script src="<?= site_url(); ?>javascript/bootstrap.min.js"></script>
 </body>
 </html>
