b3e139a1ef5ed3c5afb0c4c412ddc3239da17b02
Renato_O <renatorsdeoliveira@gmail.com>
2019-11-07 16:26:07 -0300

    Funções  de saida do formulário

--- javascript/script.js
+++ javascript/script.js
@@ -5,3 +5,17 @@ btnSubmit.addEventListener('click', event => {
     event.preventDefault();
     form.classList.add('form-hide');
 });
+
+//função que coloca o overflow hidden no body quando inicia 
+form.addEventListener('animationstart', event => {
+    if(event.animationName == 'down'){
+        document.querySelector("body").style.overflow = 'hidden';
+    }
+})
+//Função que faz o formulário sumir quando ele desce 
+form.addEventListener('animationend', event =>{
+    if(event.animationName == 'down'){
+        form.style.display = 'none';
+        document.querySelector("body").style.overflow = 'none'; // tira o overflow hidden 
+    }
+});
