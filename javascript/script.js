82e8db465c0cdea1b3cf9fa33b22a79e3e375e4f
Renato_O <renatorsdeoliveira@gmail.com>
2019-11-08 22:48:46 -0300

    Comentado  a sainda do formulario

--- javascript/script.js
+++ javascript/script.js
@@ -1,21 +1,21 @@
 const btnSubmit = document.querySelector('.btn-submit');
 const form = document.querySelector('.formulario');;
 
-btnSubmit.addEventListener('click', event => {
-    event.preventDefault();
-    form.classList.add('form-hide');
-});
+// btnSubmit.addEventListener('click', event => {
+//     event.preventDefault();
+//     form.classList.add('form-hide');
+// });
 
 //função que coloca o overflow hidden no body quando inicia 
-form.addEventListener('animationstart', event => {
-    if(event.animationName == 'down'){
-        document.querySelector("body").style.overflow = 'hidden';
-    }
-})
+// form.addEventListener('animationstart', event => {
+//     if(event.animationName == 'down'){
+//         document.querySelector("body").style.overflow = 'hidden';
+//     }
+// })
 //Função que faz o formulário sumir quando ele desce 
-form.addEventListener('animationend', event =>{
-    if(event.animationName == 'down'){
-        form.style.display = 'none';
-        document.querySelector("body").style.overflow = 'none'; // tira o overflow hidden 
-    }
-});
+// form.addEventListener('animationend', event =>{
+//     if(event.animationName == 'down'){
+//         form.style.display = 'none';
+//         document.querySelector("body").style.overflow = 'none'; // tira o overflow hidden 
+//     }
+// });
