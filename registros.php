7f99814126293c11a22ee85ee7b32f2baf07770f
Renato_O <renatorsdeoliveira@gmail.com>
2019-11-10 14:33:20 -0300

    Trocado a variavel global e colocado segurança no filtro

--- registros.php
+++ registros.php
@@ -4,16 +4,20 @@
     
 
     if (isset($_POST['nome'])){
-        $nome =  $_POST['nome'];
+        // $nome =  $_POST['nome'];
+        $nome = filter_input(INPUT_POST,'nome', FILTER_SANITIZE_SPECIAL_CHARS);
     }
     if (isset($_POST['email'])){
-        $email = $_POST['email'];
+        //$email = $_POST['email'];
+        $email = filter_input(INPUT_POST,'email', FILTER_VALIDATE_EMAIL);
     }
     if (isset($_POST['profissao'])){
-        $profissao = $_POST['profissao'];
+        // $profissao = $_POST['profissao'];
+        $profissao = filter_input(INPUT_POST, 'profissao', FILTER_SANITIZE_STRING);
     }
     if (isset($_POST['telefone'])){
-       $telefone = $_POST['telefone'];
+        // $telefone = $_POST['telefone'];
+        $telefone = filter_input(INPUT_POST, 'telefone', FILTER_SANITIZE_NUMBER_INT);
     }
 
 
@@ -25,6 +29,7 @@
     $sql = "INSERT INTO usuarios(nome, email, telefone, profissao)VALUES('$nome','$email','$telefone', '$profissao')";
     if(mysqli_query($link, $sql)){
         header('Location: index.php');
+        die();
     }else{
         $erro = "Erro ao registrar o usuario";
         echo $erro;
