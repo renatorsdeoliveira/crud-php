ec0824336dff4f59f52d3e9990179ecf8c4a3ae0
Renato_O <renatorsdeoliveira@gmail.com>
2019-11-17 09:47:24 -0300

    Backup do banco de dados

--- crud.sql
+++ crud.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Community- MySQL GUI v8.22 
-MySQL - 5.5.5-10.4.8-MariaDB : Database - crud
+MySQL - 5.5.5-10.4.6-MariaDB : Database - crud
 *********************************************************************
 */
 
@@ -15,6 +15,24 @@ MySQL - 5.5.5-10.4.8-MariaDB : Database - crud
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`crud` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE `crud`;
+
+/*Table structure for table `usuarios` */
+
+DROP TABLE IF EXISTS `usuarios`;
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(150) DEFAULT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `profissao` varchar(150) DEFAULT NULL,
+  `telefone` varchar(150) DEFAULT NULL,
+  `data` datetime DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+/*Data for the table `usuarios` */
+
+insert  into `usuarios`(`id`,`nome`,`email`,`profissao`,`telefone`,`data`) values (1,'Luiz','luiz@gmail.com','estudante','(62) 99652-5252','2019-11-15 19:27:35'),(2,'Thais','thais@gmail.com','secretaria','(61) 93233-2323','2019-11-15 19:31:53');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
